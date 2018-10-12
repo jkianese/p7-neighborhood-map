6505498441daee9f2322f07d5e82ad155628bd56
Jim Kianese <jkianese@gmail.com>
2018-10-12 05:03:52 -0400

    clean up code
    clean up code

--- src/DisMap.js
+++ src/DisMap.js
@@ -28,7 +28,7 @@ class DisMap extends Component {
   }
 
   loadMap = () => { // script on index.html. Can I ref that?
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ&callback=initMap")
+    loadScript("https://maps.googleapis.com/maps/api/js?key=&callback=initMap")
     window.initMap = this.initMap
   }
 
@@ -36,16 +36,16 @@ class DisMap extends Component {
   getVenues = () => {
     const endPoint = "https://api.foursquare.com/v2/venues/explore?"
     const parameters = {
-      client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
-      client_secret: "N1HLP0MSWYJJATAS3CBQTTSZ2WLME5RB2TAUWHGE2UXZ5A1E",
-      query: "attractions", // WT used "food"
+      client_id: "",
+      client_secret: "",
+      query: "attractions",
       near: "Disney World",
       v: "20180323"
     }
     
-    // From Walthrough video series
+    
     // Run: npm install axios
-    axios.get(endPoint + new URLSearchParams(parameters)) //URLSearchParams is actual function, don't change
+    axios.get(endPoint + new URLSearchParams(parameters))
       .then(response => {
         this.setState({
           venues: response.data.response.groups[0].items
